7df813349aa37d8dca8547ee8667f4dee7b46645
unknown <biruk@WINDOWS-5QR3M22.am.corp.amgen.com>
2021-09-21 11:09:07 +0200

    lektion 5

---
+++ lektion5-09212021/Functions/script.js
@@ -0,0 +1,55 @@
+// // Scope
+
+// let age = 34; // global scope
+
+// if(true) {
+  //   let age = 50; // local scope
+  //   let name = 'Joakim';
+  
+  
+  //   if(true) {
+    //     let age = 100;
+    //     let name = 'Jeanette';
+    //     console.log('inne i kodblock 1.2', age, name);
+    //     var globalt = 'var';
+    //   }
+    
+    //   console.log('inne i 1a kodblocket', age);
+    //   console.log('inne i 1a kodblocket', name);
+    
+    // }
+    
+    // if(true) {
+      //   let age = 66;
+      //   let name = 'Hans';
+      //   console.log('inne i 2a kodblocket', age);
+      //   console.log('inne i 2a kodblocket', name);
+      // }
+      
+      // console.log('utanför något block', age);
+      // console.log('utanför något block', name);
+      // console.log('utanför något block', globalt);
+      
+      
+      
+      
+      // Funktioner
+      
+      // Deklarera en funktion
+      
+      function hej() {
+        console.log('hej på dig');
+      }
+      
+      const greet = function() {
+        console.log('hur är läget?')
+      }
+      
+      
+      hej();
+      greet();
+      
+      
+      
+      
+      // Kalla på en funktion
